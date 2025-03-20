7ef146de09f48e1ae7c176c9ff3aec1a1e77d2fc
cleifson <cl.raujo1@gmail.com>
2025-03-20 18:05:39 -0300

    feat(api): add voip-service integration

---
+++ api/app/Infra/Services/VoipService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Infra\Services;
+
+use Twilio\Rest\Client;
+
+class VoipService
+{
+    private Client $twilio;
+
+    public function __construct()
+    {
+        $sid = env('TWILIO_SID');
+        $token = env('TWILIO_AUTH_TOKEN');
+        $this->twilio = new Client($sid, $token);
+    }
+
+    public function makeCall(string $to, string $from): array
+    {
+        try {
+            $call = $this->twilio->calls->create(
+                '+'. $to,
+                $from,
+                [
+                    'url' => 'https://handler.twilio.com/twiml/EH6d6ab0f8fe67c480bb97b6415eadcc1e'
+                ]
+            );
+
+            return [
+                'sid' => $call->sid,
+                'to' => $call->to,
+                'from' => $call->from,
+                'status' => $call->status,
+                'date_created' => $call->dateCreated,
+                'date_updated' => $call->dateUpdated,
+                'duration' => $call->duration,
+                'price' => $call->price,
+            ];
+
+        } catch (\Exception $e) {
+            throw new \Exception('Erro ao fazer a chamada: ' . $e->getMessage());
+        }
+    }
+}
