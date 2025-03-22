e5491323f5023b2b204bfae8d6359e34f4e1b43e
cleifson <cl.raujo1@gmail.com>
2025-03-22 14:56:08 -0300

    feat(front): add apexcharts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,9 +9,12 @@ import router from './router'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 
+import VueApexCharts from 'vue3-apexcharts'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Vue3Toastify, { autoClose: 3000 })
+app.use(VueApexCharts)
 app.mount('#app')
