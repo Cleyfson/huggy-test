3445691a7ef259169dc95e315bf36fbc525acccd
cleifson <cl.raujo1@gmail.com>
2025-03-22 15:15:34 -0300

    feat(front): add navigation to insights page

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="bg-gray-50 min-h-screen p-6">
     <div class="max-w-4xl mx-auto">
-      <h1 class="text-xl font-medium text-gray-800 mb-4">Contatos</h1>
+      <div class="flex justify-between items-center px-1 py-2">
+        <h1 class="text-xl font-medium text-gray-800 mb-4">Contatos</h1>
+        <button 
+          @click="goToInsights"
+          class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-md text-sm text-gray-700 flex justify-between items-center"
+        >
+          <h1>Dados</h1>
+          <ArrowRight class="pl-2"/>
+        </button>
+      </div>
 
       <ContactCard>
         <template #header>
@@ -27,6 +36,10 @@
   import SearchBar from '@/components/contact/SearchBar.vue'
   import ContactTable from '@/components/contact/ContactTable.vue'
   import ContactAddModal from '@/components/contact/ContactAddModal.vue'
+  import { ArrowRight } from 'lucide-vue-next';
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
 
   const searchQuery = ref('')
   const showAddContactAddModal = ref(false)
@@ -40,6 +53,10 @@
   const editContact = (contact) => {
     selectedContact.value = contact
     showAddContactAddModal.value = true
+  }
+
+  const goToInsights = () => {
+    router.push('/insights')
   }
 </script>
   
